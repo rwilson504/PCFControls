aa719cc69ab1ce2edff93b3b2134d99e12099b12
Rick Wilson <me@richardawilson.com>
2019-10-31 16:00:14 -0400

    Initial Commit

---
+++ ColorPicker/ColorPicker/ColorPicker.tsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import { TextField} from "office-ui-fabric-react/lib/TextField";
+import { ColorPicker } from "office-ui-fabric-react/lib/ColorPicker";
+import { Callout, DirectionalHint } from "office-ui-fabric-react/lib/Callout";
+import { DefaultButton, IButtonStyles } from 'office-ui-fabric-react/lib/Button';
+import { IColor } from 'office-ui-fabric-react/lib/Color'
+import { Stack, IStackTokens } from 'office-ui-fabric-react/lib/Stack';
+import { getTheme, ITheme, IStyle } from "@uifabric/styling";
+
+export interface IColorPickerCompProps {
+    inputValue: string,    
+    onColorChange: (color: string) => void,
+}
+
+export interface IColorPickerCompState extends React.ComponentState, IColorPickerCompProps{
+    //for properties that do not need to be passed from the PCF control to the React control
+    //put them in the State as apposed to the Props
+    displayColorPicker: boolean,
+    colorPickerButtonRef: React.RefObject<HTMLDivElement>
+}
+
+//sytle for out Stack
+const stackTokens: IStackTokens = { 
+    childrenGap: 10, 
+    padding: 5 };
+
+//get the theme so we can use it later to style non Office-UI content
+const theme: ITheme = getTheme();
+
+export class ColorPickerComp extends React.Component<IColorPickerCompProps, IColorPickerCompState>{
+    constructor(props: IColorPickerCompProps){
+        super(props);
+
+        //sets the initial state of the component
+        this.state = {
+            inputValue: props.inputValue,
+            onColorChange: props.onColorChange,
+            displayColorPicker: false,            
+            colorPickerButtonRef: React.createRef()
+        };          
+    }    
+
+    public render(): JSX.Element {
+        //css for the color picker button container
+        const colorButtonContainerSytles: React.CSSProperties = {
+            border: `1px solid ${theme.semanticColors.inputBorder}`,
+            padding: '4px'
+        }
+        
+        //css style for the color button picker
+        const colorButtonStyle: IStyle = {
+            borderWidth: '1px', 
+            borderColor: theme.semanticColors.inputBorder, 
+            backgroundColor: this.state.inputValue,                
+            minWidth: 40,
+            minHeight: 22,
+            height: 22
+        }
+
+        //css styles for the color picker button
+        const colorButtonStyles: IButtonStyles = {            
+            root: colorButtonStyle,
+            rootHovered: colorButtonStyle
+        };
+
+        return (
+            <div>            
+            <Stack horizontal tokens={stackTokens}>                                      
+                        <div ref={this.state.colorPickerButtonRef} style={colorButtonContainerSytles}>
+                            <DefaultButton ariaDescription="Select Color" styles={colorButtonStyles} onClick={this._onShowColorPickerClicked}/>
+                        </div>
+                        <Callout
+                            gapSpace={0}
+                            target={this.state.colorPickerButtonRef.current}
+                            onDismiss={this._onShowColorPickerClosed}
+                            setInitialFocus={true}
+                            hidden={!this.state.displayColorPicker}
+                            directionalHint={DirectionalHint.rightCenter}
+                            >
+                            <ColorPicker color={this.state.inputValue} onChange={this._onChangeColorPicker} />
+                        </Callout>             
+                        <TextField disabled value={`${this.state.inputValue}`}  />
+                </Stack>
+            </div>
+        );
+    }
+    
+    //called when the color picker button is clicked
+    private _onShowColorPickerClicked = () => {
+        this.setState({
+            displayColorPicker: !this.state.displayColorPicker
+        });
+      };
+
+    //called when the color picker callout is closed
+    private _onShowColorPickerClosed = () => {
+       this.setState({
+           displayColorPicker: false,
+       });
+     };
+
+    //called when the color in the color picker has changed
+    private _onChangeColorPicker = (ev: React.SyntheticEvent<HTMLElement>, colorObj: IColor) => {       
+        let fullHexColor = `#${colorObj.hex}`;
+       this.setState({ inputValue: fullHexColor },
+                  () => {
+                      //call back to our function in the PCF ts file to update the value in the props there.
+                      this.props.onColorChange(fullHexColor);
+                  });  
+     };       
+}
---
+++ ColorPicker/ColorPicker/index.ts
@@ -0,0 +1,91 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {IColorPickerCompProps, ColorPickerComp} from './ColorPicker';
+
+export class ColorPicker implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+	// Value of the field is stored and used inside the control 
+	private _value: string;	
+	// Value for the default color;
+	private _defaultColor: string;
+	// PCF framework delegate which will be assigned to this object which would be called whenever any update happens. 
+	private _notifyOutputChanged: () => void;
+	// Reference to ComponentFramework Context object
+	private _context: ComponentFramework.Context<IInputs>;
+	// Reference to the control container HTMLDivElement
+ 	// This element contains all elements of our custom control example
+	private _container: HTMLDivElement;
+	
+	private props: IColorPickerCompProps;
+	/**
+	 * Empty constructor.
+	 */
+	constructor()
+	{
+
+	}
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		this._notifyOutputChanged = notifyOutputChanged;
+		this._context = context;
+		this._container = container;
+		this._defaultColor = context.parameters.defaultColor.raw || "#ffffff"
+		this._value = context.parameters.inputValue.raw || this._defaultColor;
+
+		this.props = {
+			inputValue : this._value,
+			onColorChange: this.colorOnChange.bind(this)			
+		}					
+	}
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void
+	{		
+		this.props.inputValue = this._value;
+		
+		ReactDOM.render(
+			React.createElement(
+				ColorPickerComp, this.props
+			), 
+			this._container
+		);
+	}
+
+	private colorOnChange(colorValue: string){
+		this._value = colorValue;
+		this._notifyOutputChanged();
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{
+		return {
+			inputValue: this._value
+		  };
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		// clean up the react control
+		ReactDOM.unmountComponentAtNode(this._container);
+	}
+}
