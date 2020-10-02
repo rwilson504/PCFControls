8f3c95157cbbf70fa5a05a6db430d9dcd62d58ff
Rick Wilson <me@richardawilson.com>
2020-10-02 15:01:00 -0400

    Init

---
+++ PDFFormFillCanvas/PDFFormFillCanvas/index.ts
@@ -0,0 +1,173 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { Base64 } from 'js-base64';
+// import * as JSON5 from 'json5'
+import {Base64Encode} from 'base64-stream';
+import * as ImageType from 'image-type'
+import { PDFButton, PDFCheckBox, PDFDocument, PDFDropdown, PDFForm, PDFImage, PDFRadioGroup, PDFTextField } from 'pdf-lib';
+import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+
+export class PDFFormFillCanvas implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	private _notifyOutputChanged: () => void;
+	private _output: any;
+	private _outputErrorMessage: string;	
+
+	constructor()
+	{
+
+	}
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		context.mode.trackContainerResize(true);
+		this._notifyOutputChanged = notifyOutputChanged;			
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public async updateView(context: ComponentFramework.Context<IInputs>): Promise<void>
+	{		
+		var params = context.parameters;
+		if (!params.pdfTemplate?.raw || !params.fillPDF.raw) return;
+				
+		this._outputErrorMessage = "";
+
+		try {
+
+			this._output = `data:application/pdf;base64,${await this.PDFLibCreation(context)}`;								
+			this._notifyOutputChanged();
+		}
+		catch(error)
+		{
+			this._outputErrorMessage = `ERROR: The PDF form fill ran into an error.  MESSAGE: ${error}`
+			this._output = null;	
+		}
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{	
+		let output : IOutputs = {
+			pdfOutput : this._output || null,
+			outputErrorMessage: this._outputErrorMessage,
+			outputError: !!this._outputErrorMessage,
+			fillPDF: false
+		};		
+
+		return output;
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		// Add code to cleanup control if necessary
+	}
+
+	public async PDFLibCreation(pcfContext: ComponentFramework.Context<IInputs>):Promise<string>{
+		var params = pcfContext.parameters;
+		var dataSet = pcfContext.parameters.formDataSet;
+
+		const pdfDoc = await PDFDocument.load(this.convertStringToBinary(params.pdfTemplate.raw!));
+
+		const form = pdfDoc.getForm();
+		await this.parseDataSet(dataSet, form, pdfDoc);
+	
+		const pdfBytes = await pdfDoc.save()
+		return Base64.fromUint8Array(pdfBytes);
+	}
+
+	public async parseDataSet(dataSet: ComponentFramework.PropertyTypes.DataSet, form: PDFForm, pdfDoc: PDFDocument)
+	{
+		let totalRecordCount = dataSet.sortedRecordIds.length;
+		for (let i = 0; i < totalRecordCount; i++) {
+			var recordId = dataSet.sortedRecordIds[i];
+			var record = dataSet.records[recordId] as DataSetInterfaces.EntityRecord;
+			
+			let fieldName = record.getValue('name') as string;
+			let fieldData = record.getValue('data') || '';
+
+			await this.fillField(fieldName, fieldData, form, pdfDoc);
+		}
+	}
+
+	public async fillField(name: string, data: any, form: PDFForm, pdfDoc: PDFDocument)
+	{
+		let fieldType = form.getFieldMaybe(name);
+		if (!fieldType) return;
+		switch(fieldType.constructor.name)
+		{			
+			case PDFTextField.name: 
+				let textField = form.getTextField(name);
+				textField.setText(data);
+				break;
+			case PDFCheckBox.name:
+				let checkboxField = form.getCheckBox(name);				
+				if (this.strToBool(data)) {
+					checkboxField.check();
+				}
+				else{					
+					checkboxField.uncheck();
+				}
+				break;
+			case PDFRadioGroup.name:
+				let radioButtonField = form.getRadioGroup(name);
+				let radioButtonOptions = radioButtonField.getOptions();
+				if (radioButtonOptions.find(option => option === data)) radioButtonField.select(data);				
+				break;
+			case PDFDropdown.name:
+				let dropdownField = form.getDropdown(name);
+				let dropdownOptions = dropdownField.getOptions();
+				if (dropdownOptions.find(option => option === data)) dropdownField.select(data);
+				break;
+			case PDFButton.name:
+				const imageArray = this.convertStringToBinary(data);				
+				let imageType = ImageType(imageArray);
+				if (imageType?.ext === 'jpg')
+				{
+					const imageBytes = await pdfDoc.embedJpg(data);
+					let buttonField = form.getButton(name);
+					buttonField.setImage(imageBytes);
+				}
+				else if(imageType?.ext === 'png'){
+					const imageBytes = await pdfDoc.embedPng(data);
+					let buttonField = form.getButton(name);
+					buttonField.setImage(imageBytes);
+				}			
+				break;
+		}
+	}
+
+	public convertStringToBinary = (pdfString:string) => {
+		const BASE64_MARKER = ';base64,';
+		const base64Index = pdfString.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+		const base64 = pdfString.substring(base64Index);
+		return Base64.toUint8Array(base64);
+	};
+
+	public strToBool(s: string)
+	{
+		// will match one and only one of the string 'true','1', or 'on' rerardless
+		// of capitalization and regardless off surrounding white-space.
+		//
+		let regex=/^\s*(true|1|on|checked|yes)\s*$/i
+		return regex.test(s);
+	}
+}
---
+++ PDFFormFillCanvas/README.md
@@ -0,0 +1,35 @@
+This component will allow you to fill in a PDF Form within a Canvas App without the use of a premium flow connector.  The component utilizes the [PDF-LIB](https://pdf-lib.js.org/) open source library.
+
+![PDF Form Fill for Canvas](./images/PDFFormFill.png)
+
+## Installation/Usage
+Click on the image below to access the instructional video on how to create a simple Canvas app using the component.
+
+[![Instruction Video](./images/YouTube.png)](https://www.youtube.com/watch?v=b-jn_qPPcVs)
+
+### Additional Information
+
+* Make sure you have enabled PCF components for Canvas apps in your environment.  For instructions on that [Click Here](https://docs.microsoft.com/en-us/powerapps/developer/component-framework/component-framework-for-canvas-apps)
+
+* In the Power Apps Editor ribbon navigate to **Insert -> Custom -> Import Components**
+    ![Import Component](./images/ImportComponent.png)
+
+* On the Import Component screen select the **Code** tab and Import the **RAW! PDF Form Fill (Canvas)**
+    ![Import Component](./images/ImportComponentFromCode.png)
+
+* Open the **Code components** area in the Insert panel and add the **RAW! PDF Form Fill (Canvas)** component to the form.  The control itself will just be a blank space on the form.  It is suggested you rename the Component you dropped on the form to make it easier to access.
+
+* Click on the Component on the form and set the Properties for the component.
+    * pdfTemplate (string): This will be the file name which the browser will download the content as.
+    * fillPDF (bool): Set this to true to kick off the PDF form fill.
+    * Items (Collection): This is the collection of items which will fill the form.  It needs to be a JSON Collection where each object has two fields name and data (these are case sensitive).  The name field will be the name of the PDF form field and data is the content that will be put in that field.
+    ``
+        ClearCollect(TestCollect, 
+        {name: "Name", data: ThisItem.'Full Name'},
+        {name: "Email", data: ThisItem.Email},
+        {name: "CreditHold", data:  Text(ThisItem.'Credit Hold')},
+        {name: "ContactEmail", data: Text(ThisItem.'Do not allow Emails')},
+        {name: "PaymentTerms", data: Text(ThisItem.'Payment Terms')},
+        {name: "Photo", data: ImageJSON});
+    ``
+    * OnChange: Use this to output the final pdf to a variable that be can used by another component such as the PDF Preview.
